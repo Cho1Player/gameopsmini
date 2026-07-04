c3ba2a567ac9be1fefc0af95bc2abd7e5fbd387a
Cho1Player <iychoi99@gmail.com>
2026-07-04 18:40:02 +0900

    feat : REST Client 테스트 파일 생성
    [작업 내용]
    - api-test.http 테스트 파일 생성
    - API 포트 고정(http만)
    - 포트는 5000으로 고정함
    
    [테스트]
    - api-test.http 내 모든 엔드포인트 테스트 완료.
    - 포트 5000 고정 확인 완료.
    
    [관련 이슈]
    - 없음.

--- GameOpsMini.Api/obj/Debug/net10.0/GameOpsMini.Api.AssemblyInfo.cs
+++ GameOpsMini.Api/obj/Debug/net10.0/GameOpsMini.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameOpsMini.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47d4c88f0cfa4a780b92b58f3c9c5d743242c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8114727852c50c49288b122ba070006d0e49490b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameOpsMini.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameOpsMini.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
